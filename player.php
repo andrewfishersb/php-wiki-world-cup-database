b9238af69146867569295b669d761739ad6341be
Andrew Fisher <andrewfisher@Andrews-MacBook-Pro.local>
2017-02-27 12:28:52 -0800

    Now each player is created as an object and while all the players are created at once

--- player.php
+++ player.php
@@ -1,14 +1,52 @@
 <?php
 
+/**
+ * Created by PhpStorm.
+ * User: andrewfisher
+ * Date: 2/27/17
+ * Time: 11:48 AM
+ */
 class player
 {
-    var $name;
-    var $age;
-    var $nationality;
-    var $club;
-    var $clubCountry;
+    private $name;
+    private $age;
+    private $nationality;
+    private $club;
+    private $clubCountry;
+    private $playerId;
 
-
+    /**
+     * player2 constructor.
+     * @param $name
+     * @param $age
+     * @param $nationality
+     * @param $club
+     * @param $clubCountry
+     * @param $playerId
+     */
+//     function __construct($name, $age, $nationality, $club, $clubCountry, $playerId)
+//    {
+//        $this->setName($name);
+//        $this->setAge($age);
+//        $this->setNationality($nationality);
+//        $this->setClub($club);
+//        $this->setClubCountry($clubCountry);
+//        $this->playerId = $playerId;
+//    }
+    function __construct($name,$age,$nationality,$clubAndCountry,$playerId){
+        $this->setName($name);
+        $this->setAge($age);
+        $this->setNationality($nationality);
+        $this->setClub($clubAndCountry);
+        $this->setClubCountry($clubAndCountry);
+        $this->setPlayerId($playerId);
+//        $this->name = $name;
+//        $this->age=$age;
+//        $this->nationality=$nationality;
+//        $this->club= $clubAndCountry;
+//        $this->clubCountry = $clubAndCountry;
+//        $this->playerId= $playerId;
+    }
 
     //will send in the player and it will take their html and extract that specific information
     /**
@@ -25,8 +63,16 @@ class player
     /**
      * @param mixed $name
      */
-    public function setName($name)
+    public function setName($playerName)
     {
+
+        //ALSO CHECK FOR [] Edder Delgado[97] Honduras
+
+        $name = strip_tags($playerName);
+        if(strpos($name,"(c)")>0){
+            $name = substr($name,0,strpos($name,"(c)")-1);
+        }
+
         $this->name = $name;
     }
 
@@ -41,10 +87,15 @@ class player
     }
 
     /**
+     * Clearly coverts (aged 25) to 25. Could have made a more compact method but it would have made a long mess
      * @param mixed $age
      */
-    public function setAge($age)
+    public function setAge($playerAge)
     {
+        $ageString = strip_tags($playerAge);
+        $posOfAged = strpos($ageString,"aged ");
+        $substringAge = substr($ageString,$posOfAged+5,2);
+        $age = (int) $substringAge;
         $this->age = $age;
     }
 
@@ -63,6 +114,12 @@ class player
      */
     public function setNationality($nationality)
     {
+
+
+        //an array of all the countries, force the array to be size of 32 ( can change depending on the world cup/euro size) loop through
+        //<li class="toclevel-2 tocsection-2"><a href="#Brazil"><span class="tocnumber">1.1</span> <span class="toctext">Brazil</span></a></li>
+        //check that it does not contain the word Group. Before it gets to the other headers the array limit should stop it, then during the final looping
+        //count to 23, afterwards switch to the next index...
         $this->nationality = $nationality;
     }
 
@@ -79,8 +136,14 @@ class player
     /**
      * @param mixed $club
      */
-    public function setClub($club)
+    public function setClub($playerClub)
     {
+        $club = strip_tags($playerClub);
+
+        if(strpos($club,"[")>0){
+            $club = substr($club,0,strpos($club,"["));
+        }
+
         $this->club = $club;
     }
 
@@ -88,7 +151,6 @@ class player
      * @return mixed
      */
 
-    //flagicon
     public function getClubCountry()
     {
         return $this->clubCountry;
@@ -97,8 +159,35 @@ class player
     /**
      * @param mixed $clubCountry
      */
-    public function setClubCountry($clubCountry)
+    public function setClubCountry($playerClubCountry)
     {
+//MIGHT BE ABLE TO SIMPLIFY
+        $doc = new DOMDocument();
+        $doc->loadHTML($playerClubCountry);
+        $xml = simplexml_import_dom($doc);
+        $titles =$xml->xpath('//a');
+
+        $clubCountry = $titles[0]['title'];
+
+
         $this->clubCountry = $clubCountry;
     }
+
+
+    /**
+     * @return mixed
+     */
+    public function getPlayerId()
+    {
+        return $this->playerId;
+    }
+
+    /**
+     * @param mixed $playerId
+     */
+    public function setPlayerId($playerId)
+    {
+        $this->playerId = $playerId;
+    }
+
 }
--- wiki.php
+++ wiki.php
@@ -1,98 +1,109 @@
 <?php
+//include("player.php");
+include("player.php");
+
+//two attemps. One is to have everything in one big loop and add the object at the end into an array. or 2. Read in all the players, then format and create objects
+
 $html = file_get_contents('https://en.wikipedia.org/wiki/2014_FIFA_World_Cup_squads');
  preg_match_all('/<tr>(.*?)<\/tr>/s',$html,$matches);
 
 
 $players =array();
 
+//$randomPlayer = new player();
+$playerId=0;
 for ($i=0;$i<=sizeof($matches[0]);$i++){
+
+
+    //check that they are a player since all players have the word aged in their table data
     if(strpos($matches[0][$i], 'aged') !== false){
-        array_push($players,$matches[0][$i]);
+
+
+        //find the name in the th tag, if matched $name[0] hold the name placed <>here</> $name[1] will contain things such as what is in the tag ie styling
+        preg_match('/<th(.*?)<\/th>/s',$matches[0][$i],$findName);
+//        $randomPlayer->setName($findName[0]);
+
+
+        // '/<td(.*?)\/td>/s' Use this to split up all other info data follows(see if different for captain = |0->number|1->position|2->age|3->apps|4->club/country(will need for extraction)
+        //double array [0][x] x is the column i am taking from
+        preg_match_all('/<td(.*?)<\/td>/s',$matches[0][$i],$playerData);
+
+
+//        $randomPlayer->setAge();
+//        $randomPlayer->setClubCountry($playerData[0][4]);
+//        $randomPlayer->setClub();
+
+        $name =  $findName[0];
+        $age = $playerData[0][2];
+        $nationality = "Brazil";
+        $clubAndClubCountry = $playerData[0][4];
+
+
+
+        array_push($players,new player($name,$age,$nationality,$clubAndClubCountry,$playerId));
+//        array_push($players,new player("Andrew",25,"British American","Portland", "Portland FC",$playerId));
+        $playerId++;
+
     }
 }
 
-//dont think is needed
-$counter=1;
+foreach ($players as $player){
+    echo "My name is " .  $player->getName() . " I am " . $player->getAge() . " years old. I live in " . $player->getClubCountry() . " and I play for " . $player->getClub() . " and my ID is: " . $player->getPlayerId() ."<br>";
+
+}
+
+
+//echo $players[0]->getName();
+//echo sizeof($players);
+//echo $players[0]->getName();
+//echo $players[0]->getAge();
+//echo $players[0]->getNationality();
+//echo $players[0]->getClub();
+//echo $players[0]->getClubCountry();
+//echo $players[0]->getPlayerId();
+//print_r($players[0]);
+//echo "<br>" . $players[0]->getName();
+//echo "<br>" . $players[0]->get();
+//echo "<br>" . $players[0]->getName();
+//echo "<br>" . $players[0]->getName();
+//echo "<br>" . $players[0]->getName();
+
+
 
 //736 players
-$playerId = random_int(0,sizeof($players));
-//$playerId = 352;
+//$playerId = random_int(0,sizeof($players));
+//$playerId = 71;
 
-
-
-//find the name in the th tag, if matched $name[0] hold the name placed <>here</> $name[1] will contain things such as what is in the tag ie styling
-preg_match('/<th(.*?)<\/th>/s',$players[$playerId],$findName);
-$name = strip_tags($findName[0]);
-if(strpos($name,"(c)")>0){
-    $name = substr($name,0,strpos($name,"(c)")-1);
-}
-//echo $name;
-//this concludes the get name portion
-
-
-
-
-
-// '/<td(.*?)\/td>/s' Use this to split up all other info data follows(see if different for captain = |0->number|1->position|2->age|3->apps|4->club/country(will need for extraction)
-//double array [0][x] x is the column i am taking from
-
-preg_match_all('/<td(.*?)<\/td>/s',$players[$playerId],$playerData);
-
-
-//for ($i =0;$i<sizeof($playerData[0]);$i++){
-//    echo $playerData[0][$i];
-//}
-
-
-//age
-$ageString = strip_tags($playerData[0][2]);
-$posOfAged = strpos($ageString,"aged ");
-$substringAge = substr($ageString,$posOfAged+5,2);
-$age = (int) $substringAge;
-//echo $age;
+//
+////Back up in case of a constructor failing
+//$randomPlayer->setPlayerId($i);
+//
+////find the name in the th tag, if matched $name[0] hold the name placed <>here</> $name[1] will contain things such as what is in the tag ie styling
+//preg_match('/<th(.*?)<\/th>/s',$matches[0][$i],$findName);
+//$randomPlayer->setName($findName[0]);
+//
+//
+//// '/<td(.*?)\/td>/s' Use this to split up all other info data follows(see if different for captain = |0->number|1->position|2->age|3->apps|4->club/country(will need for extraction)
+////double array [0][x] x is the column i am taking from
+//preg_match_all('/<td(.*?)<\/td>/s',$matches[0][$i],$playerData);
+//
+//$randomPlayer->setAge($playerData[0][2]);
+//$randomPlayer->setClubCountry($playerData[0][4]);
+//$randomPlayer->setClub($playerData[0][4]);
 
 
 
 
-//club name
-//echo strip_tags($playerData[0][4]); //this gets the club name -- look into pulling out the whole name SS Lazio for Klose
-
-
-//echo $playerData[0][4];
-$doc = new DOMDocument();
-$doc->loadHTML($playerData[0][4]);
-$xml = simplexml_import_dom($doc);
-$titles =$xml->xpath('//a');
-
-$club;
-$country;
-
-
-//$t1=false;
-//$t2 = false;
-foreach ($titles as $title){
-        if(empty($country)){
-            $country = $title['title'];
-        }
-        else if(empty($club)){
-            //gets name
-            $clubName = $title['title'];
-echo $clubName;
-            //check that only uses letters
-            if(!ctype_alpha($clubName)){//MAY NEED A DIFFERENT METHOD TO CHECK FOR OUR LETTERS
-                $club = $clubName;
-                echo "got here";
-            }else{
-                $club = strip_tags($playerData[0][4]);
-            }
-
-        }
-}
 
 
 
 
-echo "My name is " . $name . " I am " . $age . " years old. I live in " . $country . " and I play for " . $club . " and my ID is: " . $playerId;
+
+
+
+
+
+
 
 
 
@@ -102,7 +113,6 @@ echo "My name is " . $name . " I am " . $age . " years old. I live in " . $count
 //for ($i=552;$i<575;$i++){
 //    echo $i . '->>' . $players[$i];
 //}
-
 
 
 
@@ -124,3 +134,5 @@ echo "My name is " . $name . " I am " . $age . " years old. I live in " . $count
 //maybe a php player class
 //name, age?, nationality, team, country of team
 //methods to extract each eventually
+
+
